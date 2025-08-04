327158804c715bd5a6da919f25ec9017daa7060d
Rudnei Santiago <rudnei.santiago@gmail.com>
2025-08-04 15:14:09 -0300

    Implementando animação

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,10 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Animated, Image, Text, TouchableOpacity, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { useLocalSearchParams, useRouter } from "expo-router";
 
 import { styles } from "../../styles/HomeStyle";
-import batsinal from "../../../assets/batsignal.png";
-import { useEffect, useState } from "react";
-import { useLocalSearchParams, useRouter } from "expo-router";
 import { Call } from "../../models/call";
+import batsinal from "../../../assets/batsignal.png";
 
 export default function Home() {
   const [imageHidden, setImageHidden] = useState(true);
@@ -15,14 +15,52 @@ export default function Home() {
 
   useEffect(() => {
     if (quem) {
-      setTextHidden(false);
-
-      setTimeout(() => {
-        setImageHidden(false);
-        setTextHidden(true);
-      }, 5000);
+      showMessage()
+      showImage()
     }
   }, []);
+
+  const fadeTextAnim = useRef(new Animated.Value(0)).current
+  const fadeImageAnim = useRef(new Animated.Value(0)).current
+
+  const fadeIn = (value: Animated.Value) => {
+    Animated.timing(value, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true
+    }).start()
+  }
+
+  const fadeOut = (value: Animated.Value) => {
+    Animated.timing(value, {
+      toValue: 0,
+      duration: 2000,
+      useNativeDriver: true
+    }).start()
+  }
+
+  const sleep = (ms: number) => {
+    return new Promise<void>((resolve, _) => {
+      setTimeout(() => {
+        resolve()
+      }, ms);
+    })
+  }
+
+  const showMessage = async () => {
+    setTextHidden(false)
+    fadeIn(fadeTextAnim)
+    await sleep(3000)
+    fadeOut(fadeTextAnim)
+    await sleep(2000)
+    setTextHidden(true)
+  }
+
+  const showImage = async () => {
+    await sleep(5000)
+    setImageHidden(false)
+    fadeIn(fadeImageAnim)
+  }
 
   const askForHelp = () => {
     router.navigate("/Help");
@@ -30,15 +68,24 @@ export default function Home() {
 
   return (
     <View style={styles.container}>
-      <View style={[styles.imageContainer, imageHidden ? styles.hidden : null]}>
-        <Image style={styles.image} source={batsinal} />
-      </View>
+      {
+        quem && !imageHidden && (
+          <Animated.View style={{ opacity: fadeImageAnim }}>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={batsinal} />
+            </View>
+          </Animated.View>
+
+        )
+      }
       {quem && !textHidden && (
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>Batman 🦇, {quem} está pedindo ajuda</Text>
-          <Text style={styles.text}>{porQue}</Text>
-          <Text style={styles.text}>{algoMais}</Text>
-        </View>
+        <Animated.View style={{ opacity: fadeTextAnim }}>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Batman 🦇, {quem} está pedindo ajuda</Text>
+            <Text style={styles.text}>{porQue}</Text>
+            <Text style={styles.text}>{algoMais}</Text>
+          </View>
+        </Animated.View>
       )}
       {textHidden && (
         <TouchableOpacity onPress={askForHelp}>
